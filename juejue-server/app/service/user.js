6e4cbbe7380706c224748f97ff68591db54d1a5f
ChaunceyCHI <Alex_loveyou@yeah.net>
2022-08-05 23:33:34 +0800

    后端功能ADD-后端用户添加
    在后端实现用户添加功能，并关联至数据库

---
+++ juejue-server/app/service/user.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Server = require('egg').Controller;
+
+class UserService extends Server {
+// 通过用户名获取用户信息
+  async getUserByName(username) {
+    const { app } = this;
+    try {
+      const result = await app.mysql.get('user', { username });
+      return result;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  // 注册
+  async register(params) {
+    const { app } = this;
+    try {
+      const result = await app.mysql.insert('user', params);
+      return result;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+}
+
+module.exports = UserService;
