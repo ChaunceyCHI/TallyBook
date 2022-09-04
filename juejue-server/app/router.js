847244dd39db89e3364041443adc18a3894315cc
ChaunceyCHI <Alex_loveyou@yeah.net>
2022-09-04 22:10:11 +0800

    后端接口实现。

--- juejue-server/app/router.js
+++ juejue-server/app/router.js
@@ -14,4 +14,8 @@ module.exports = app => {
   router.post('/api/upload', _jwt, controller.upload.upload); // 上传图片
   router.post('/api/bill/add', _jwt, controller.bill.add);// 添加账单
   router.get('/api/bill/list', _jwt, controller.bill.list);// 获取账单列表
+  router.get('/api/bill/detail', _jwt, controller.bill.detail); // 获取详情
+  router.post('/api/bill/update', _jwt, controller.bill.update); // 账单更新
+  router.post('/api/bill/delete', _jwt, controller.bill.delete); // 删除账单
+  router.get('/api/bill/data', _jwt, controller.bill.data); // 获取数据
 };
