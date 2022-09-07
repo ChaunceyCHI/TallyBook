7b61d11441335fab639fc5389baba61bd7e1717a
ChaunceyCHI <Alex_loveyou@yeah.net>
2022-09-08 00:36:48 +0800

    搭建前端环境
    Vite2.0 + React + ZarmUI 搭建前端H5开发环境。完成了CSS局部引用的优化，利用插件vite-plugin-style-import使打包大小减小很多。

--- react-vite-h5/vite.config.js
+++ react-vite-h5/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createStyleImportPlugin } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    createStyleImportPlugin(
+      {
+        libs: [
+          {
+            libraryName: 'zarm',
+            exModules: true,
+            resolveStyle: (name) => {
+              return `zarm/es/${name}/style/css`
+            }
+          }
+        ]
+      }
+    )
+  ]
 })
